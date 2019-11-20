68f6ec35456de2fe869a9214edce72753ad76005
Maamun <mamun.mokni@gmail.com>
2019-11-20 20:42:06 +0400

    Binding multiple form controls

--- ult-react-co/src/Form.js
+++ ult-react-co/src/Form.js
@@ -1,16 +1,39 @@
 import React, { useState } from "react";
 
 const Form = () => {
-  const [firstName, setFirstName] = useState("");
+  const [formState, setFormState] = useState({
+    firstName: "",
+    lastName: ""
+  });
+
   const onChangehandler = e => {
-    setFirstName(e.target.value);
+    setFormState({
+      ...formState,
+      [e.target.name]: e.target.value
+    });
+    console.log(formState);
   };
+
   return (
     <form>
-      <span>{firstName}</span>
+      <span>{`Your First Name is ${formState.firstName} ${formState.lastName}`}</span>
       <br />
       <label htmlFor="firstName">First Name</label>
-      <input id="firstName" value={firstName} onChange={onChangehandler} />
+      <input
+        id="firstName"
+        name="firstName"
+        value={formState.firstName}
+        onChange={onChangehandler}
+      />
+      <br />
+
+      <label htmlFor="lastName">last Name</label>
+      <input
+        id="lastName"
+        name="lastName"
+        value={formState.lastName}
+        onChange={onChangehandler}
+      />
     </form>
   );
 };
