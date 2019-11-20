fe81263455c046a8f0e281c0e968b5e1064ceb35
Maamun <mamun.mokni@gmail.com>
2019-11-20 18:49:52 +0400

    Setter function callback

--- ult-react-co/src/StopWatch.js
+++ ult-react-co/src/StopWatch.js
@@ -1,15 +1,15 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect } from "react";
 const StopWatch = () => {
   const [time, setTime] = useState(0);
   const [counterActive, setCounterActive] = useState(true);
-  const currentTime = useRef(0);
+
   useEffect(() => {
     let interval = null;
     /* interval is declared inside the effect hook so we do not need to put it inside the dependency aray */
     if (counterActive) {
       interval = setInterval(() => {
-        currentTime.current++;
-        setTime(currentTime.current);
+        setTime(t => t + 1);
+        /* in order to get rid of this Ref we use the callback version of setter function, so react will inject the correct version of that state into that callback function for us. */
       }, 1000);
     }
 
@@ -24,10 +24,15 @@ const StopWatch = () => {
     setTime(0);
     setCounterActive(false);
   };
+
+  const formattedTime = new Date(time * 1000).toISOString().substr(11, 8);
+  /* - This code has to do with the rendering of our view, this is not a side effect therefore it has to be part of the main render cycle of the component.
+  - Side effects in useEffects does not have to do with the main render cycle of the component. Also this means that we are running this conversion function in every single render. Note that this kind of things is normal and perfectly fine in react. React is fast enough to process most of what we through at it. It's only when something becomes a problem that we try and optimise it. Often trying to optimize things too early in react create more problems than it actually solves.  */
+
   return (
     <div className="container">
       <h1>Ultimate StopWatch</h1>
-      <p>{time}</p>
+      <p>{formattedTime}</p>
       <button
         type="button"
         aria-pressed={!counterActive}
